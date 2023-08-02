235b4d7567238d4d8fa786ed8177ea4dc3e3c602
Etravers <102576227+emtraverso@users.noreply.github.com>
2023-08-02 16:14:10 +0200

    Add files via upload
    First commit

---
+++ So_long/src/free_close.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_close.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etravers <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 19:08:29 by etravers          #+#    #+#             */
+/*   Updated: 2023/02/20 19:08:31 by etravers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_freemap(t_mlx *init)
+{
+	t_tile	**tilemap;
+	int		y;
+
+	tilemap = init->map;
+	y = init->y;
+	while (y--)
+	{
+		free(*init->map);
+		init->map++;
+	}
+	free(tilemap);
+}
+
+int	ft_close(t_mlx *init)
+{
+	mlx_destroy_window(init->mlx, init->win);
+	mlx_destroy_display(init->mlx);
+	free(init->mlx);
+	ft_freemap(init);
+	exit(0);
+	return (0);
+}
---
+++ So_long/src_bonus/error_bonus.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etravers <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 19:11:54 by etravers          #+#    #+#             */
+/*   Updated: 2023/02/20 19:11:55 by etravers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+void	ft_error_map(char *error, char *map)
+{
+	free(map);
+	ft_putstr_fd(error, 2);
+	exit (0);
+}
+
+int	ft_close_error(t_mlx *init)
+{
+	ft_freemap(init);
+	exit(0);
+	return (0);
+}
+
+void	ft_error_border(t_mlx *init)
+{
+	ft_putstr_fd("Error! Invalid map\n", 2);
+	ft_close_error(init);
+}
